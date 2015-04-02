44e7fecfdae8e14920140a19740644f156f0b356
vriyaz <vriyaz@gmail.com>
2015-04-01 20:47:49 -0500

    FirstApp - first commit

--- AlphaApp/ViewController.swift
+++ AlphaApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+
+    let messages : [String] = ["may the force be with you", "live long and prosper", "to infinity and beyond", "space is big. you just wont believe how vastly, hugely, mind-bogglingly big it is"]
+
+    var index : Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doButtonTap(sender: AnyObject) {
+
+        println("button touched! \(index) : \(self.messages[index])")
+
+        self.messageLabel.text = self.messages[index++]
+
+        index %= self.messages.count
+    }
 
 }
 
